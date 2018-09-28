6197445420ddb6440f482d2715d5b7694abe4491
haiaimi <wanghai1997@outlook.com>
2018-09-28 17:22:33 +0800

    BFBRT算法

--- leetcode_Exer/leetcode_Exer/main.cpp
+++ leetcode_Exer/leetcode_Exer/main.cpp
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+//二叉树结点
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
+
 class Solution {
 public:
 	//求最长有效括号匹配
@@ -156,7 +165,104 @@ public:
 	}
 };
 
+#pragma region BFBRT
+const int N = 10005;
+
+int a[N];
+
+//插入排序
+void InsertSort(int a[], int l, int r)
+{
+	for (int i = l + 1; i <= r; i++)
+	{
+		if (a[i - 1] > a[i])
+		{
+			int t = a[i];
+			int j = i;
+			while (j > l && a[j - 1] > t)
+			{
+				a[j] = a[j - 1];
+				j--;
+			}
+			a[j] = t;
+		}
+	}
+}
+
+//寻找中位数的中位数
+int FindMid(int a[], int l, int r)
+{
+	if (l == r) return l;
+	int i = 0;
+	int n = 0;
+	for (i = l; i < r - 5; i += 5)
+	{
+		InsertSort(a, i, i + 4);
+		n = i - l;
+		swap(a[l + n / 5], a[i + 2]);
+	}
+
+	//处理剩余元素
+	int num = r - i + 1;
+	if (num > 0)
+	{
+		InsertSort(a, i, i + num - 1);
+		n = i - l;
+		swap(a[l + n / 5], a[i + num / 2]);
+	}
+	n /= 5;
+	if (n == l) return l;
+	return FindMid(a, l, l + n);
+}
+
+//进行划分过程
+int Partion(int a[], int l, int r, int p)
+{
+	swap(a[p], a[l]);
+	int i = l;
+	int j = r;
+	int pivot = a[l];
+	while (i < j)
+	{
+		while (a[j] >= pivot && i < j)
+			j--;
+		a[i] = a[j];
+		while (a[i] <= pivot && i < j)
+			i++;
+		a[j] = a[i];
+	}
+	a[i] = pivot;
+	return i;
+}
+
+int BFPRT(int a[], int l, int r, int k)
+{
+	int p = FindMid(a, l, r);    //寻找中位数的中位数
+	int i = Partion(a, l, r, p);
+
+	int m = i - l + 1;
+	if (m == k) return a[i];
+	if (m > k)  return BFPRT(a, l, i - 1, k);
+	return BFPRT(a, i + 1, r, k - m);
+}
+
+#pragma endregion
+
 int main()
 {
+	int n, k;
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++)
+		scanf_s("%d", &a[i]);
+
+	scanf_s("%d", &k);
+	printf_s("The %d th number is : %d\n", k, BFPRT(a, 0, n - 1, k));
+
+	for (int i = 0; i < n; i++)
+		printf_s("%d ", a[i]);
+
+	puts("");
+	system("pause");
+
 	return 0;
 }
