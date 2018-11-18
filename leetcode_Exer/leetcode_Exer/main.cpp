c38997a600262962211f233a2e16da3fa46496af
haiaimi <wanghai1997@outlook.com>
2018-11-18 17:38:41 +0800

    通配符匹配（动态规划、贪心算法）

--- leetcode_Exer/leetcode_Exer/main.cpp
+++ leetcode_Exer/leetcode_Exer/main.cpp
@@ -1616,6 +1616,49 @@ public:
 		if (p[0] != s[0])return false;
 		else return isMatch(s.substr(1), p.substr(1));
 	}
+
+	//动态规划法
+	bool isMatch_dp(string s, string p) {
+		int m = s.size(), n = p.size();
+		vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+		dp[0][0] = true;
+		for (int i = 1; i <= n; ++i) {
+			if (p[i - 1] == '*') dp[0][i] = dp[0][i - 1];
+		}
+		for (int i = 1; i <= m; ++i) {
+			for (int j = 1; j <= n; ++j) {
+				if (p[j - 1] == '*') {
+					dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+				}
+				else {
+					dp[i][j] = (s[i - 1] == p[j - 1] || p[j - 1] == '?') && dp[i - 1][j - 1];
+				}
+			}
+		}
+		return dp[m][n];
+	}
+
+	//贪婪算法
+	bool isMatch_Greedy(char *s, char *p) {
+		char *scur = s, *pcur = p, *sstar = NULL, *pstar = NULL;
+		while (*scur) {
+			if (*scur == *pcur || *pcur == '?') {
+				++scur;
+				++pcur;
+			}
+			else if (*pcur == '*') {
+				pstar = pcur++;
+				sstar = scur;
+			}
+			else if (pstar) {
+				pcur = pstar + 1;
+				scur = ++sstar;
+			}
+			else return false;
+		}
+		while (*pcur == '*') ++pcur;
+		return !*pcur;
+	}
 };
 
 #pragma region BFBRT
