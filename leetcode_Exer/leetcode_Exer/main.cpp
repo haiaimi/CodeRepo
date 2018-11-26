83f76707cf517466d1a95c80c563b29c51ee81f4
haiaimi <wanghai1997@outlook.com>
2018-11-26 21:35:31 +0800

    跳跃游戏（贪心算法）

--- leetcode_Exer/leetcode_Exer/main.cpp
+++ leetcode_Exer/leetcode_Exer/main.cpp
@@ -1677,6 +1677,20 @@ public:
 		}
 		return dp.back();
 	}
+
+	//贪心算法，正确做法
+	int jump_Greedy(vector<int>& nums) {
+		int res = 0, n = nums.size(), i = 0, cur = 0;
+		while (cur < n - 1) {
+			++res;
+			int pre = cur;
+			for (; i <= pre; ++i) {
+				cur = max(cur, i + nums[i]);
+			}
+			if (pre == cur) return -1; // May not need this
+		}
+		return res;
+	}
 };
 
 #pragma region BFBRT
