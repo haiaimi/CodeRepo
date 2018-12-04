59550f958fc4915330e0d2a0991c109d075a870e
haiaimi <wanghai1997@outlook.com>
2018-12-04 22:28:38 +0800

    买卖股票的最佳时机 II

--- leetcode_Exer/leetcode_Exer/main.cpp
+++ leetcode_Exer/leetcode_Exer/main.cpp
@@ -1713,6 +1713,23 @@ public:
 
 		return res;
 	}
+
+	//买卖股票的最佳时机 II
+	//使用贪心算法
+	int maxProfit(vector<int>& prices) {
+		if (prices.size() <= 1)return 0;
+		int profit = 0, pre = prices[0];
+
+		for (int i = 1; i < prices.size(); ++i)
+		{
+			if (prices[i] > pre)
+				profit += prices[i] - pre;
+
+			pre = prices[i];
+		}
+
+		return profit;
+	}
 };
 
 #pragma region BFBRT
